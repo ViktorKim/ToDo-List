3af18bab56201c553925dc6d4f08f0b43992e30f
Viktor Kim <fake2spam@gmail.com>
2018-09-11 00:40:09 +0300

    implamented saving todoList name in local storage

--- src/components/AddListModal.vue
+++ src/components/AddListModal.vue
@@ -10,15 +10,15 @@
                     <v-container grid-list-md>
                         <v-layout wrap>
                             <v-flex xs12>
-                                <v-text-field label="Todo List name" type="text" required></v-text-field>
+                                <v-text-field label="Todo List name" :error-messages="errorMessage" v-model="newTodo" type="text" required clearable autofocus></v-text-field>
                             </v-flex>
                         </v-layout>
                     </v-container>
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" flat @click.native="dialog = false">Discard</v-btn>
-                    <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
+                    <v-btn color="blue darken-1" flat @click.native="discard">Discard</v-btn>
+                    <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
@@ -31,8 +31,27 @@
         data () {
             return {
                 dialog: false,
+                newTodo: '',
+                errorMessage: '',
             }
-        }
+        },
+        methods: {
+            save: function () {
+                let _message = window.saveTodoList(this.newTodo);
+                if (_message.status === 'error'){
+                    this.dialog = true;
+                    this.errorMessage = _message.text;
+                } else {
+                    this.dialog = false;
+                    this.newTodo = '';
+                }
+            },
+            discard: function () {
+                this.dialog = false;
+                this.newTodo = '';
+                this.errorMessage = '';
+            }
+        },
     }
 </script>
 
