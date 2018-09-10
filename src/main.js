21cf9f567cf95732e5ee6739d1c737e38f6afe8c
Viktor Kim <fake2spam@gmail.com>
2018-09-11 00:39:13 +0300

    Added helper function saveTodoList, add local storage init function

--- src/main.js
+++ src/main.js
@@ -3,8 +3,49 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
+
+/**
+ * Init local storage
+ *
+ * Create todoLists Local Storage if not exists
+ */
+(function() {
+    if (localStorage.getItem('todoLists') === null){
+        localStorage.setItem('todoLists', "{}");
+    }
+}());
+
+/**
+ * Save TodoList in local storage
+ *
+ * @param todoListName
+ *
+ * @return _message - error message object
+ */
+window.saveTodoList = function(todoListName) {
+    let _message = {};
+    if (todoListName.trim() === ''){
+        _message = {
+            status: 'error',
+            text: 'This field is required',
+        };
+    } else {
+        let _storageData = JSON.parse(localStorage.getItem('todoLists'));
+        if (_storageData[todoListName] !== undefined){
+            _message = {
+                status: 'error',
+                text: 'This list already exists',
+            };
+        } else {
+            _storageData[todoListName] = '';
+            localStorage.setItem('todoLists', JSON.stringify(_storageData));
+        }
+    }
+
+    return _message;
+};
